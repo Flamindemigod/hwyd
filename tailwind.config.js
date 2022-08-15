cbf78c870fd9741f85eed3f7b6f2d03bb42c7d32
Savio Andrade <andradesavio9073@gmail.com>
2022-08-15 09:25:12 +0200

    added tailwind css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,24 @@
-/** @type {import('tailwindcss').Config} */ 
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          100: "#ded7f6",
+          200: "#bcafed",
+          300: "#9b86e5",
+          400: "#795edc",
+          500: "#5836d3",
+          600: "#462ba9",
+          700: "#35207f",
+          800: "#231654",
+          900: "#120b2a"
+        },
+      }
+    },
   },
   plugins: [],
 }
