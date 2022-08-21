f8b9a0448d35b495e3782e130a53b9958d4e9ca2
Savio Andrade <andradesavio9073@gmail.com>
2022-08-21 16:22:23 +0200

    added calender export

--- src/components/Calender.js
+++ src/components/Calender.js
@@ -1,10 +1,12 @@
 import { Button } from '@mui/material';
-import React, { useState } from 'react'
+import React, { createRef, useState } from 'react'
 import Month from './Month'
 import { Menu, MenuItem } from '@mui/material';
 import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
 import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
+import { exportComponentAsPNG } from 'react-component-export-image';
 const Calender = ({ session }) => {
+  const ref = createRef();
   const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
   const [date, setDate] = useState(new Date());
 
@@ -17,42 +19,48 @@ const Calender = ({ session }) => {
     setAnchorEl(null)
   }
 
-  return (<div className='w-fit mx-auto mt-8'>
-    <div className='flex items-center justify-between w-full'>
-      <Button onClick={() => { setDate(new Date(date.getFullYear(), date.getMonth() - 1, date.getDate())) }}><ArrowBackIosIcon /></Button>
-      <div className='w-full text-center text-xl' 
-        aria-controls={menuOpen ? 'basic-menu' : undefined}
-        aria-haspopup="true"
-        aria-expanded={menuOpen ? 'true' : undefined} 
-        onClick={handleClick}>{`${months[date.getMonth()]} ${date.getFullYear()}`}</div>
-      <Menu
-        id="basic-menu"
-        anchorEl={anchorEl}
-        open={menuOpen}
-        anchorOrigin={{ vertical: "bottom", horizontal: "center" }}
-        transformOrigin={{vertical: "top", horizontal: "center"}}
-        onClose={handleClose}
-        MenuListProps={{
-          'aria-labelledby': 'basic-button',
-          
-        }}
-      > <MenuItem className=''>
-      <div className="flex justify-between w-full items-center">
-        <Button onClick={()=>{setDate(new Date(date.getFullYear()-1, date.getMonth(), date.getDate()))}}><ArrowBackIosIcon /></Button>
-          {date.getFullYear()}
-          <Button onClick={()=>{setDate(new Date(date.getFullYear()+1, date.getMonth(), date.getDate()))}}><ArrowForwardIosIcon /></Button>
-      </div>
-      </MenuItem>
-        <div className="grid grid-cols-3">
-        {months.map((month, index)=>(<MenuItem key={index} onClick={()=>{setDate(new Date(date.getFullYear(), index, date.getDate())); handleClose();}}>{month}</MenuItem>))}
+  return (
+    <div className='relative'>
+      <div className='w-fit mx-auto mt-8 p-8' ref={ref}>
+        <div className='flex items-center justify-between w-full'>
+          <Button onClick={() => { setDate(new Date(date.getFullYear(), date.getMonth() - 1, date.getDate())) }}><ArrowBackIosIcon /></Button>
+          <div className='w-full text-center text-xl'
+            aria-controls={menuOpen ? 'basic-menu' : undefined}
+            aria-haspopup="true"
+            aria-expanded={menuOpen ? 'true' : undefined}
+            onClick={handleClick}>{`${months[date.getMonth()]} ${date.getFullYear()}`}</div>
+          <Menu
+            id="basic-menu"
+            anchorEl={anchorEl}
+            open={menuOpen}
+            anchorOrigin={{ vertical: "bottom", horizontal: "center" }}
+            transformOrigin={{ vertical: "top", horizontal: "center" }}
+            onClose={handleClose}
+            MenuListProps={{
+              'aria-labelledby': 'basic-button',
+
+            }}
+          > <MenuItem className=''>
+              <div className="flex justify-between w-full items-center">
+                <Button onClick={() => { setDate(new Date(date.getFullYear() - 1, date.getMonth(), date.getDate())) }}><ArrowBackIosIcon /></Button>
+                {date.getFullYear()}
+                <Button onClick={() => { setDate(new Date(date.getFullYear() + 1, date.getMonth(), date.getDate())) }}><ArrowForwardIosIcon /></Button>
+              </div>
+            </MenuItem>
+            <div className="grid grid-cols-3">
+              {months.map((month, index) => (<MenuItem key={index} onClick={() => { setDate(new Date(date.getFullYear(), index, date.getDate())); handleClose(); }}>{month}</MenuItem>))}
+            </div>
+          </Menu>
+          <Button onClick={() => { setDate(new Date(date.getFullYear(), date.getMonth() + 1, date.getDate())) }}><ArrowForwardIosIcon /></Button>
         </div>
-      </Menu>
-      <Button onClick={() => { setDate(new Date(date.getFullYear(), date.getMonth() + 1, date.getDate())) }}><ArrowForwardIosIcon /></Button>
+        <div className='mx-auto w-fit'>
+          <Month date={date} session={session} />
+        </div>
+      </div>
+      <Button sx={{position:"absolute"}} className="top-0 right-0" onClick={() => exportComponentAsPNG(ref)}>
+        Export As PNG
+      </Button>
     </div>
-    <div className='mx-auto w-fit'>
-      <Month date={date} session={session} />
-    </div>
-  </div>
   )
 }
 
